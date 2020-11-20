70092c680e12cebb1468609a2e873598e648a292
Moredy <60314905+Moredy@users.noreply.github.com>
2020-11-20 19:55:06 -0300

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -3,14 +3,6 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyB1jTXAGxIwQu-n6ZO_pnStCPv149oDUDc",
-  authDomain: "websecurityapp.firebaseapp.com",
-  databaseURL: "https://websecurityapp.firebaseio.com",
-  projectId: "websecurityapp",
-  storageBucket: "websecurityapp.appspot.com",
-  messagingSenderId: "695568189148",
-  appId: "1:695568189148:web:49914f0f3d860e1d7c5619",
-  measurementId: "G-RRJBLKHDBF"
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
